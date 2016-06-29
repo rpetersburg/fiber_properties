d7aa19e12720e73a19b3c70729eb25040f8380a2
Ryan Petersburg <rpetersburg@gmail.com>
2016-06-29 17:57:20 -0400

    Added larger fiber LED test to modal noise testing

--- Modal Noise Test.py
+++ Modal Noise Test.py
@@ -2,6 +2,7 @@ import FiberProperties as FP
 import ImageAnalysis as IA
 
 LED_image = "../Alignment Images/2016-06-28/Near field normal camera distance zoomed out.tif"
+LED_image2 = "../Alignment Images/2016-03-15/NF_67_fiber.tif"
 #LED_image = "../Alignment Images/2016-06-22 FCS First Proper Alignment/LED near field.tif"
 laser_image = "../Alignment Images/2016-06-22 FCS First Proper Alignment/Laser near field.tif"
 #laser_image = "../Alignment Images/2016-03-15/NF_67_test1_3.5ms.tif"
@@ -12,11 +13,20 @@ nf_flatfield_images = ["../Alignment Images/2016-03-16/sn67_flatfield1.tif",
 
 
 LED_image_analysis = IA.ImageAnalysis(LED_image, nf_dark_images, nf_flatfield_images)
+LED_image_analysis2 = IA.ImageAnalysis(LED_image2, nf_dark_images, nf_flatfield_images)
 laser_image_analysis = IA.ImageAnalysis(laser_image, nf_dark_images, nf_flatfield_images)
 
 fpObject = FP.FiberProperties(None, LED_image_analysis, None)
 print "LED:"
-for i in xrange(20):
+for i in xrange(10):
+    i = 0.9 + 0.01 * i
+    test = fpObject.getModalNoise(i)
+    print "Factor:", i, "MN:", test
+print
+
+fpObject = FP.FiberProperties(None, LED_image_analysis2, None)
+print "LED:"
+for i in xrange(10):
     i = 0.9 + 0.01 * i
     test = fpObject.getModalNoise(i)
     print "Factor:", i, "MN:", test
@@ -25,7 +35,7 @@ print
 
 fpObject = FP.FiberProperties(None, laser_image_analysis, None)
 print "Laser:"
-for i in xrange(20):
+for i in xrange(10):
     i = 0.9 + 0.01 * i
     test = fpObject.getModalNoise(i)
     print "Factor:", i, "MN:", test
