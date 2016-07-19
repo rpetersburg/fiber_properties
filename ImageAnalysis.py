bbed5b81c5aa3ff821451ef4eb07794d18882fe0
Ryan Petersburg <rpetersburg@gmail.com>
2016-07-19 16:26:25 -0400

    Removed ImageAnalysisController and place code inside ImageAnalysis

--- ImageAnalysis.py
+++ ImageAnalysis.py
@@ -701,8 +701,9 @@ class ImageAnalysis(NumpyArrayHandler):
         """Set fiber edge pixel values
 
         Sets the left, right, top, and bottom edges of the fiber by finding where
-        the row and column sums cross the given threshold. Also sets the width
-        of the fiber by the maximum of the horizontal and vertical lengths
+        the maxima of each row and column cross the given threshold. Also sets
+        the width of the fiber by the maximum of the horizontal and vertical
+        lengths
 
         Args:
             threshold: the minimum brightness value used to determine the fiber
@@ -711,28 +712,6 @@ class ImageAnalysis(NumpyArrayHandler):
         Returns:
             None
         """
-        # row_sum = self.getRowSum(self._image_array)
-        # column_sum = self.getColumnSum(self._image_array)
-
-        # left = -1
-        # right = -1
-        # for index, value in enumerate(row_sum):
-        #     if left < 0:
-        #         if value > self._threshold:
-        #             left = index
-        #     else:
-        #         if value > self._threshold:
-        #             right = index
-        # top = -1
-        # bottom = -1
-        # for index, value in enumerate(column_sum):
-        #     if top < 0:
-        #         if value > self._threshold:
-        #             top = index
-        #     else:
-        #         if value > self._threshold:
-        #             bottom = index
-
         left = -1
         right = -1
         for index in xrange(self._image_width):
@@ -777,3 +756,56 @@ class ImageAnalysis(NumpyArrayHandler):
                                                                                 x0, y0, radius,
                                                                                 res=res),
                                        y0, x0)
+
+
+if __name__ == "__main__":
+    calibration_folder = "Calibration/TIF/"
+    image_folder = '../Alignment Images/2016-07-12/'
+
+    nf_images = []
+    for i in xrange(10):
+        #nf_images.append(image_folder + 'nf_laser_noagitation_' + str(i) + '_1.8ms.tif')
+        nf_images.append(image_folder + 'nf_laser_agitation_' + str(i) + '_1.8ms.tif') 
+    nf_dark_images = []
+    nf_ambient_images = []
+    for i in xrange(3):
+        nf_dark_images.append(calibration_folder + 'nf_dark_' + str(i) + '_10ms.tif')
+        nf_ambient_images.append(image_folder + 'nf_ambient_' + str(i) + '_1.8ms.tif')
+
+    nf_flat_images = []
+    #for i in xrange(8):
+    #    nf_flat_images.append(calibration_folder + 'nf_flat_' + str(i) + '_1ms.tif')
+
+    imAnalysis = ImageAnalysis(nf_images, nf_dark_images,
+                                             nf_flat_images, nf_ambient_images,
+                                             pixel_size=3.45,
+                                             magnification=10,
+                                             bits_adc=16)
+    tol = .2
+    test_range = 10
+
+    print 'Height:', imAnalysis.getImageHeight(), 'Width:', imAnalysis.getImageWidth()
+    imAnalysis.showImageArray()
+    print
+    print 'Centroid'
+    centroid_row, centroid_column = imAnalysis.getFiberCentroid()
+    print 'Centroid Row:', centroid_row, 'Centroid Column:', centroid_column
+    print
+    print 'Edge:'
+    center_y, center_x = imAnalysis.getFiberCenter(method='edge')
+    print 'Diameter:', imAnalysis.getFiberDiameterMicrons(method='edge'), 'microns'
+    print 'Center Row:', center_y, 'Center Column:', center_x
+    print
+    print 'Circle:'
+    center_y, center_x = imAnalysis.getFiberCenterCircleMethod(tol=tol, test_range=test_range)
+    print 'Center Row:', center_y, 'Center Column:', center_x
+    print
+    print 'Radius:'
+    center_y, center_x = imAnalysis.getFiberCenterRadiusMethod(tol=tol, test_range=test_range)
+    print 'Diameter:', imAnalysis.getFiberDiameterMicrons(method='radius'), 'microns'
+    print 'Center Row:', center_y, 'Center Column:', center_x
+    print
+    print 'Gaussian:'
+    center_y, center_x = imAnalysis.getFiberCenter(method='gaussian')
+    print 'Diameter:', imAnalysis.getFiberDiameterMicrons(method='gaussian'), 'microns'
+    print 'Center Row:', center_y, 'Center Column:', center_x
