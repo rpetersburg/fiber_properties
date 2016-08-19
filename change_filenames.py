74442e6ccce8d9b4664e3558214b64607050132c
Ryan Petersburg <rpetersburg@gmail.com>
2016-08-19 11:22:15 -0400

    Moving new change_filenames to linux

--- change_filenames.py
+++ change_filenames.py
@@ -2,7 +2,13 @@ import os
 import re
 
 for filename in os.listdir('.'):
-    if filename.startswith('nf') or filename.startswith('in'):
-        # string_list = re.split('_|\\.', filename)
-        os.rename(filename, filename[:2] + '_' + filename[2:5].zfill(3) + '.fit')
+    if filename.startswith('nf') or filename.startswith('in') or filename.startswith('ff'):
+        # os.rename(filename, filename[:2] + '_' + filename[-7:])
 
+        # string_list = re.split('_', filename)
+        # if len(string_list) > 3:
+        #     os.rename(filename, string_list[0] + '_' + string_list[2] + '_' + string_list[3])
+
+        # string_list = re.split('_|\\.', filename)
+        # if len(string_list) > 3:
+        #     os.rename(filename, string_list[0] + '_' + string_list[2].zfill(3) + '.' + string_list[3])
