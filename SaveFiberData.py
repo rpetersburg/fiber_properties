61c173f41087cc46b918721e9ca39c64d0972d5a
Ryan Petersburg <rpetersburg@gmail.com>
2016-08-25 17:57:09 -0400

    Changing file name

--- SaveFiberData.py
+++ SaveFiberData.py
@@ -65,43 +65,43 @@ if __name__ == '__main__':
     folder = base_folder + 'stability_unagitated/'
     ext = '.fit'
 
-    # in_calibration = Calibration(dark=[dark_folder + 'in_' + str(i).zfill(3) + ext for i in xrange(10)],
-    #                              ambient=[ambient_folder + 'in_' + str(i).zfill(3) + ext for i in xrange(10)],
-    #                              flat=[flat_folder + 'in_' + str(i).zfill(3) + ext for i in xrange(8)])
-    # print 'IN calibration initialized'
+    in_calibration = Calibration(dark=[dark_folder + 'in_' + str(i).zfill(3) + ext for i in xrange(10)],
+                                 ambient=[ambient_folder + 'in_' + str(i).zfill(3) + ext for i in xrange(10)],
+                                 flat=[flat_folder + 'in_' + str(i).zfill(3) + ext for i in xrange(8)])
+    print 'IN calibration initialized'
     nf_calibration = Calibration(dark=[dark_folder + 'nf_' + str(i).zfill(3) + ext for i in xrange(10)],
                                  ambient=[ambient_folder + 'nf_' + str(i).zfill(3) + '_0.001' + ext for i in xrange(10)],
                                  flat=[flat_folder + 'nf_' + str(i).zfill(3) + ext for i in xrange(8)])
     print 'NF calibration initialized'
-    # ff_calibration = Calibration(dark=[dark_folder + 'ff_' + str(i).zfill(3) + ext for i in xrange(10)],
-    #                              ambient=[ambient_folder + 'ff_' + str(i).zfill(3) + '_0.001' + ext for i in xrange(10)])
-    # print 'FF calibration initialized'
+    ff_calibration = Calibration(dark=[dark_folder + 'ff_' + str(i).zfill(3) + ext for i in xrange(10)],
+                                 ambient=[ambient_folder + 'ff_' + str(i).zfill(3) + '_0.001' + ext for i in xrange(10)])
+    print 'FF calibration initialized'
 
     num_images = 100
-    # in_images = [folder + 'in_' + str(i).zfill(3) + ext for i in xrange(num_images)]
+    in_images = [folder + 'in_' + str(i).zfill(3) + ext for i in xrange(num_images)]
     nf_images = [folder + 'nf_' + str(i).zfill(3) + ext for i in xrange(num_images)]
-    # ff_images = [folder + 'ff_' + str(i).zfill(3) + ext for i in xrange(num_images)]
+    ff_images = [folder + 'ff_' + str(i).zfill(3) + ext for i in xrange(num_images)]
 
     if PARALLELIZE:
         from multiprocessing import Pool
         from functools import partial
         pool = Pool(processes=6)
-        # pool.map(partial(saveInputData, in_calibration=in_calibration), in_images)
-        # print 'IN data initialized'
+        pool.map(partial(saveInputData, in_calibration=in_calibration), in_images)
+        print 'IN data initialized'
         pool.map(partial(saveNearFieldData, nf_calibration=nf_calibration), nf_images)
         print 'NF data initialized'
-        # pool.map(partial(saveFarFieldData, ff_calibration=ff_calibration), ff_images)
-        # print 'FF data initialized'
+        pool.map(partial(saveFarFieldData, ff_calibration=ff_calibration), ff_images)
+        print 'FF data initialized'
 
     else:    
-        # for image in in_images:
-        #     saveInputData(image, in_calibration)
-        # print 'IN data initialized'
+        for image in in_images:
+            saveInputData(image, in_calibration)
+        print 'IN data initialized'
 
         for image in nf_images:
             saveNearFieldData(image, nf_calibration)
         print 'NF data initialized'
 
-        # for image in ff_images:
-        #     saveFarFieldData(image, ff_calibration)
-        # print 'FF data initialized'
+        for image in ff_images:
+            saveFarFieldData(image, ff_calibration)
+        print 'FF data initialized'
