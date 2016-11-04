4beca805f1cd15d2ee677bb7ce434f4153aa983e
Ryan Petersburg <rpetersburg@gmail.com>
2016-11-04 17:23:57 -0400

    Removing pyc files

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='FiberProperties',
-    version='0.1',
+    version='1.0',
     author='Ryan Petersburg',
     author_email='ryan.petersburg@yale.edu',
-    description=('Image Analysis for Fiber Characterization Station'),
+    description='Image Analysis for Fiber Characterization Station',
     license='MIT',
     url='https://github.com/rpetersburg/FiberProperties',
     packages=['FiberProperties']
